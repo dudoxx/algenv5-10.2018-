2282d2cfcdc857bce2af79feb6b160195c3b996b
Kamil Duljas <kduljas@SOLWIT.local>
2018-10-11 14:03:55 +0200

    release 10.2018 version 5

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ enum state
 
 int main(int argc, char* argv[])
 {
- /*   if(argc == 2)
+    if(argc == 2)
         if(!strcmp( argv[ 1 ], "--info" ))
         {
             cout<<"Ver 5 (11.10.2018)";
@@ -30,38 +30,31 @@ int main(int argc, char* argv[])
         return 2;
     }
 
-    string file ="[v5]convert-";
-    ifstream read;
-    read.open(argv[1]);
-    if(read.fail())
-    {
-        cout<<"Brak pliku lub nie mozna go otworzyc. \n"
-        << "Sprawdz czy program i plik sa w tym samym katalogu lub czy sciezka do pliku jest prawidlowa";
-        return 3;
-    }
-    //ofstream saveFile(file+argv[1]);
-*/
+
     string line;    //buffer for first line in file
     ifstream rawFile;   //input file
     rawFile.open("1.txt");
 
+    string file ="[v5]convert-";
+    rawFile.open(argv[1]);
     if(rawFile.fail())
     {
-        cout<<"Blad odczytu - rawFile.fail() return true";
-        return 1;
+        cout<<"Brak pliku lub nie mozna go otworzyc. \n"
+        << "Sprawdz czy program i plik sa w tym samym katalogu lub czy sciezka do pliku jest prawidlowa";
+        return 3;
     }
 
     getline(rawFile,line);
     cout<<"Ilosc alleli: "<<count(line.begin(),line.end(),'\t')-3<<'\n';
 
     Sample sample;
-    ofstream saveFile("work.txt");  //output file
+    ofstream saveFile(file+argv[1]); //output file
 
     int status = 0;
 
     while(1)
     {
-            status = sample.parseStream(rawFile);
+        status = sample.parseStream(rawFile);
         if(status == END_SAMPLE)
         {
             sample.toSave(saveFile);
